3d159cc4ddf67bedf450d5ffc459153bc5e7b586
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-02-07 00:59:44 +0200

    setup apollo server

---
+++ server.js
@@ -0,0 +1,34 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// The GraphQL schema
+const typeDefs = gql`
+  type User {
+    userName: String!
+    email: String!
+  }
+  type Query {
+    getUsers: [User]!
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    getUsers: () => {
+      const users = [
+        { userName: "john", email: "john@gmail.com" },
+        { userName: "jane", email: "jane@gmail.com" },
+      ];
+      return users;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
