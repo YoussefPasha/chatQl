345922df72d744fd49c3d84790cef4b1cfe36fdf
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-02-19 16:41:55 +0200

    retrieve all messages from spacific user

--- graphQl/resolvers/messages.js
+++ graphQl/resolvers/messages.js
@@ -1,8 +1,32 @@
 const { UserInputError, AuthenticationError } = require("apollo-server");
-
+const { Op } = require("sequelize");
 const { Message, User } = require("../../models");
 
 module.exports = {
+  Query: {
+    getMessages: async (parent, { from }, { user }) => {
+      try {
+        if (!user) throw new AuthenticationError("Unauthenticated");
+        const otherUser = await User.findOne({
+          where: { userName: from },
+        });
+        if (!otherUser) throw new UserInputError("User not found");
+        const userNames = [user.userName, otherUser.userName];
+
+        const messages = await Message.findAll({
+          where: {
+            from: { [Op.in]: userNames },
+            to: { [Op.in]: userNames },
+          },
+          order: [["createdAt", "DESC"]],
+        });
+        return messages;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+  },
   Mutation: {
     sendMessage: async (parent, { to, content }, { user }) => {
       try {
